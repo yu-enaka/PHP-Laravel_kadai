93c085cedb933293db78f4b17f8445de4609547c
”yu” <enaka@opzt.net>
2020-07-06 03:49:58 +0000

    PHP/Laravel 08

---
+++ app/Http/Controllers/Admin/NewsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class NewsController extends Controller
+{
+  // 以下を追記
+  public function add()
+  {
+      return view('admin.news.create');
+  }
+
+}
