b1af34b00e602f8d6763c84819a7654bab2c3826
”yu” <enaka@opzt.net>
2020-07-06 04:38:26 +0000

    PHP/Laravel 10

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,14 +1,13 @@
-CTYPE html>
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MyNews</title>
+        <title>課題No.4 add</title>
     </head>
     <body>
-        <h1>Myニュース作成画面</h1>
+        <h1>課題No.4 add 確認画面</h1>
     </body>
 </html>
-
