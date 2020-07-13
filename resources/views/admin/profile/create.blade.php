000caa1f84d3db76187edbd825b86ece67f62f8a
”yu” <enaka@opzt.net>
2020-07-13 16:24:20 +0000

    PHP/Laravel 11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,15 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+{{--   課題No.5   --}}
+@extends('layouts.admin')
 
-        <title>課題No.4 add</title>
-    </head>
-    <body>
-        <h1>課題No.4 add 確認画面</h1>
-    </body>
-</html>
+@section('title', 'ニュースの新規作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>課題No.5</h2>
+            </div>
+        </div>
+    </div>
+@endsection
+{{--   課題No.5   --}}
